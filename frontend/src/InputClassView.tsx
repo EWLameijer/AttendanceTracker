205e64d1e3a7fc696028539cce25d771ff1fdeb6
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2023-12-15 15:43:34 +0100

    Changed name to InputClass to avoid confusion due to ScheduledClass

---
+++ frontend/src/InputClassView.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Attendance, Class, addExtraData } from './Class';
+import { BASE_URL, capitalize, dateOptions, toYYYYMMDD } from './utils';
+
+const ScheduleClassView = () => {
+    const [chosenClass, setChosenClass] = useState<Class>()
+    const [date, setDate] = useState<Date | undefined>()
+
+    return  <>
+        <h2>Hallo!</h2>
+        <h3>Voer nieuwe les in:</h3 >
+    </>
+}
+
+export default ScheduleClassView;
