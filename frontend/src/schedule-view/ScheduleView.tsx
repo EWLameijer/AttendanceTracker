bc5073cb587aa47904144b50d71897544e3e01ca
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-02-02 15:15:07 +0100

    Attempting refactor of handle methods

--- frontend/src/schedule-view/ScheduleView.tsx
+++ frontend/src/schedule-view/ScheduleView.tsx
@@ -1,19 +1,23 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { BASE_URL, capitalize, dateOptions, toYYYYMMDD } from "../utils";
+import { BASE_URL, toYYYYMMDD } from "../utils";
 import { Group } from "../admin-view/Group";
 import { Teacher } from "./Teacher";
 
 const ScheduleView = () => {
   const [groups, setGroups] = useState<Group[]>([]);
   const [teachers, setTeachers] = useState<Teacher[]>([]);
+  const [selection, setSelection] = useState({
+    teacher: String,
+    group: String,
+    date: String,
+  });
   const [selectedTeacher, setSelectedTeacher] = useState<String>();
   const [selectedGroup, setSelectedGroup] = useState<String>();
   const [selectedDate, setSelectedDate] = useState<String>(
     toYYYYMMDD(new Date())
   );
 
-  // - Capture selected date in a variable
   // - Set up API path to save selected data
 
   useEffect(() => {
@@ -21,14 +25,15 @@ const ScheduleView = () => {
       setTeachers(response.data);
       setSelectedTeacher(response.data[0].name);
     });
-  }, []);
-
-  useEffect(() => {
     axios.get(`${BASE_URL}/admin-view/chantal/groups`).then((response) => {
       setGroups(response.data);
       setSelectedGroup(response.data[0].name);
     });
   }, []);
+
+  const handleSelectionChanged = (event: any) => {
+    setSelection({ ...selection, [event.target.name]: event.target.value });
+  };
 
   const handleDateChanged = (event: any) => {
     setSelectedDate(event.target.value);
@@ -49,8 +54,9 @@ const ScheduleView = () => {
   const submit = () => {
     axios
       .post(`${BASE_URL}/scheduled_class`, {
-        a: String,
-        b: String,
+        selectedDate,
+        selectedTeacher,
+        selectedGroup,
       })
       .then(() => {})
       .catch(() => {});
@@ -64,6 +70,7 @@ const ScheduleView = () => {
       <p>Kies een datum:</p>
       <input
         id="inputDate"
+        name="date"
         type="date"
         value={selectedDate.toString()}
         onChange={handleDateChanged}
@@ -71,14 +78,14 @@ const ScheduleView = () => {
       <br />
 
       <p>Kies een leraar:</p>
-      <select id="teacher" onChange={handleSelectedTeacher}>
+      <select id="teacher" name="teacher" onChange={handleSelectedTeacher}>
         {teachers.map((value: Teacher, index: number) => (
           <option key={index}>{value.name}</option>
         ))}
       </select>
 
       <p>Kies een groep:</p>
-      <select id="group" onChange={handleSelectedGroup}>
+      <select id="group" name="group" onChange={handleSelectedGroup}>
         {groups.map((value: Group, index: number) => (
           <option key={index}>{value.name}</option>
         ))}
