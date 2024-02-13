3f6d4e794276e208673be7a9025657cca8908881
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-02-13 15:58:31 +0100

    Fixed the error in the if statement that prevents duplicate entries.

--- backend/src/main/java/nl/itvitae/attendancetracker/scheduledclass/ScheduledClassController.java
+++ backend/src/main/java/nl/itvitae/attendancetracker/scheduledclass/ScheduledClassController.java
@@ -1,10 +1,8 @@
 package nl.itvitae.attendancetracker.scheduledclass;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import nl.itvitae.attendancetracker.BadRequestException;
 import nl.itvitae.attendancetracker.group.GroupRepository;
-import nl.itvitae.attendancetracker.personnel.Personnel;
 import nl.itvitae.attendancetracker.personnel.PersonnelRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -50,16 +48,14 @@ public class ScheduledClassController {
             throw new BadRequestException("Please provide the date in this format: \"YYYY-MM-DD\"");
         }
 
-        //if (scheduledClassRepository.findByDateAndTeacher(localDate, possiblePersonnel).isEmpty()) {
-        scheduledClassRepository.save(new ScheduledClass(
-                possibleGroup.get(),
-                possiblePersonnel.get(),
-                localDate
-        ));
-        //}
+        if (scheduledClassRepository.findByDateAndTeacher(localDate, possiblePersonnel.get()).isEmpty()) {
+            scheduledClassRepository.save(new ScheduledClass(
+                    possibleGroup.get(),
+                    possiblePersonnel.get(),
+                    localDate
+            ));
+        }
 
-
-        // tijdelijke 200 status zodat ik kan kijken of het werkt
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
