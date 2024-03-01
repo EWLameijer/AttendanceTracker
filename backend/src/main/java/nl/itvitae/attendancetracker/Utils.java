1685e2bf62b346eddbe7c2a0eb19fa0f58c29756
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-03-01 19:10:41 +0100

    Made the exception more specific.

--- backend/src/main/java/nl/itvitae/attendancetracker/Utils.java
+++ backend/src/main/java/nl/itvitae/attendancetracker/Utils.java
@@ -1,12 +1,13 @@
 package nl.itvitae.attendancetracker;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class Utils {
     public static LocalDate parseLocalDateOrThrow(String input) {
         try {
             return LocalDate.parse(input);
-        } catch (Exception e) {
+        } catch (DateTimeParseException e) {
             throw new BadRequestException("Please provide the date in this format: \"YYYY-MM-DD\"");
         }
     }
