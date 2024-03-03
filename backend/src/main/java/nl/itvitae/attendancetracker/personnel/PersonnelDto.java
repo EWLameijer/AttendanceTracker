fd558ea9e4a331c713f9e5810442fe0c5fcb5b7e
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-03-03 15:50:25 +0100

    Refactored enum to be saved as a string in DB, still be an enum in backend, and be a string in frontend.

--- backend/src/main/java/nl/itvitae/attendancetracker/personnel/PersonnelDto.java
+++ backend/src/main/java/nl/itvitae/attendancetracker/personnel/PersonnelDto.java
@@ -6,8 +6,8 @@ import nl.itvitae.attendancetracker.student.StudentDto;
 import java.util.List;
 import java.util.UUID;
 
-public record PersonnelDto(UUID id, String name, ATRole role) {
+public record PersonnelDto(UUID id, String name, String role) {
     public static PersonnelDto from(Personnel personnel) {
-        return new PersonnelDto(personnel.getId(), personnel.getName(), personnel.getRole());
+        return new PersonnelDto(personnel.getId(), personnel.getName(), personnel.getRole().name());
     }
 }
