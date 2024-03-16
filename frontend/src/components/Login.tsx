59d9820eb97425f52187e369267ff715748a0cf0
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-03-16 16:14:32 +0100

    Removed Role import from Login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,7 +2,6 @@ import { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Role from "./shared/Role";
 
 const Login = () => {
   const [loginData, setLoginData] = useState({ username: "", password: "" });
