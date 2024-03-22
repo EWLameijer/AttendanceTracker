7bc210d99c0dd7a8620d5880593a14f84c3c7880
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-03-22 23:26:21 +0100

    Added my rough idea for upgrading this page. Already copied the group implementation from original.

--- frontend/src/schedule-view/ScheduleView.tsx
+++ frontend/src/schedule-view/ScheduleView.tsx
@@ -123,6 +123,117 @@ const ScheduleView = () => {
           Opslaan
         </button>
       </div>
+
+      <div>
+        <p>Kies een groep:</p>
+        <select>
+          <option>51 C#</option>
+          <option>52 Java</option>
+          <option>53 CyberSecurity</option>
+        </select>
+      </div>
+
+      <div>
+        <p>Kies een begin- en einddatum:</p>
+        <table>
+          <tr>
+            <td>Begindatum</td>
+            <td>
+              <input type="date" />
+            </td>
+            <td>Einddatum</td>
+            <td>
+              <input type="date" />
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div>
+        <p>Selecteer lesdagen en wie die dag hun leraar is:</p>
+        <table>
+          <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>
+              <input type="checkbox" />
+            </td>
+          </tr>
+          <tr>
+            <td>Maandag</td>
+            <td>Dinsdag</td>
+            <td>Woensdag</td>
+            <td>Donderdag</td>
+            <td>Vrijdag</td>
+          </tr>
+          <tr>
+            <td>
+              <select>
+                <option></option>
+                <option>Wubbo</option>
+                <option>Kenji</option>
+                <option>Mathijs</option>
+              </select>
+            </td>
+            <td>
+              <select>
+                <option></option>
+                <option>Wubbo</option>
+                <option>Kenji</option>
+                <option>Mathijs</option>
+              </select>
+            </td>
+            <td>
+              <select>
+                <option></option>
+                <option>Wubbo</option>
+                <option>Kenji</option>
+                <option>Mathijs</option>
+              </select>
+            </td>
+            <td>
+              <select>
+                <option></option>
+                <option>Wubbo</option>
+                <option>Kenji</option>
+                <option>Mathijs</option>
+              </select>
+            </td>
+            <td>
+              <select>
+                <option></option>
+                <option>Wubbo</option>
+                <option>Kenji</option>
+                <option>Mathijs</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div>
+        <p>
+          Eventueel kunnen we nog een aparte invoerpagina maken om vakantiedagen
+          in AT te kunnen opslaan. Want die zijn waarschijnlijk hetzelfde voor
+          heel ITvitae. Vervolgens kun je met deze knop de hele fase en/of de
+          gehele opleiding opslaan per groep. Het valt vast te regelen dat op
+          basis hiervan roosters, lokaaloverzichten en dergelijke automatisch
+          gegenereerd kunnen worden door Attendance Tracker. Op deze manier hoef
+          je dan niet meer al die Excelbestanden te maken, plus al die variaties
+          per groep.
+        </p>
+        <button>Sla periode op.</button>
+      </div>
     </form>
   );
 };
