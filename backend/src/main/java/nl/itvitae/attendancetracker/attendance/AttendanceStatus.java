9f9b3b4f547fd3e534a4e4917132aaacb1685b78
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-03-23 13:34:42 +0100

    Backend seems updated now (including database). Now to work on frontend

--- backend/src/main/java/nl/itvitae/attendancetracker/attendance/AttendanceStatus.java
+++ backend/src/main/java/nl/itvitae/attendancetracker/attendance/AttendanceStatus.java
@@ -1,3 +1,3 @@
 package nl.itvitae.attendancetracker.attendance;
 
-public enum AttendanceStatus {ABSENT_WITH_NOTICE, ABSENT_WITHOUT_NOTICE, PRESENT, SICK, WORKING_FROM_HOME}
+public enum AttendanceStatus {ABSENT_WITH_NOTICE, ABSENT_WITHOUT_NOTICE, LATE, PRESENT, SICK, WORKING_FROM_HOME}
