6d34ebfd775f6bf3a1a2d65398e6edbf0f93fce8
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-05-04 15:21:43 +0200

    Changed BadRequest exceptions to ResponseEntity("why", 400).

--- backend/src/main/java/nl/itvitae/attendancetracker/scheduledclass/ScheduledClassController.java
+++ backend/src/main/java/nl/itvitae/attendancetracker/scheduledclass/ScheduledClassController.java
@@ -1,7 +1,6 @@
 package nl.itvitae.attendancetracker.scheduledclass;
 
 import lombok.RequiredArgsConstructor;
-import nl.itvitae.attendancetracker.BadRequestException;
 import nl.itvitae.attendancetracker.group.GroupRepository;
 import nl.itvitae.attendancetracker.personnel.PersonnelRepository;
 import org.springframework.http.HttpStatus;
@@ -36,20 +35,20 @@ public class ScheduledClassController {
             LocalDate localDate = parseLocalDateOrThrow(potentialClass.dateAsString());
 
             UUID possibleTeacher = UUID.fromString(potentialClass.teacherId());
-            var teacher = personnelRepository.findById(possibleTeacher).orElseThrow(() ->
-                    new BadRequestException("Teacher not found"));
+            var teacher = personnelRepository.findById(possibleTeacher);
+            if (teacher.isEmpty()) return new ResponseEntity<>("Leraar bestaat niet", HttpStatus.BAD_REQUEST);
 
-            if (scheduledClassRepository.findByDateAndTeacher(localDate, teacher).isPresent()) {
+            if (scheduledClassRepository.findByDateAndTeacher(localDate, teacher.get()).isPresent()) {
                 return new ResponseEntity<>(MessageFormat.format(
                         "De geselecteerde leraar is niet beschikbaar op {0}.",
                         potentialClass.dateAsString()), HttpStatus.BAD_REQUEST);
             }
 
             UUID possibleGroup = UUID.fromString((potentialClass.groupId()));
-            var group = groupRepository.findById(possibleGroup).orElseThrow(() ->
-                    new BadRequestException("Group not found"));
+            var group = groupRepository.findById(possibleGroup);
+            if (group.isEmpty()) return new ResponseEntity<>("Groep bestaat niet", HttpStatus.BAD_REQUEST);
 
-            validClasses.add(new ScheduledClass(group, teacher, localDate));
+            validClasses.add(new ScheduledClass(group.get(), teacher.get(), localDate));
         }
 
         for (ScheduledClass validClass : validClasses) {
