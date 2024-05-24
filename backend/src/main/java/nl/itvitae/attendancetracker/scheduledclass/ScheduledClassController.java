ec946670fe67b74843b5729a32c2c49085eaf751
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-05-24 17:23:56 +0200

    Simplified null checks.

--- backend/src/main/java/nl/itvitae/attendancetracker/scheduledclass/ScheduledClassController.java
+++ backend/src/main/java/nl/itvitae/attendancetracker/scheduledclass/ScheduledClassController.java
@@ -37,22 +37,22 @@ public class ScheduledClassController {
         for (ScheduledClassInputDto potentialClass : listNewClasses) {
             LocalDate localDate = parseLocalDateOrThrow(potentialClass.dateAsString());
 
-            var teacher = personnelRepository.findById(potentialClass.teacherId());
-            if (teacher.isEmpty()) throw new BadRequestException("Leraar bestaat niet");
+            var teacher = personnelRepository.findById(potentialClass.teacherId())
+                    .orElseThrow(() -> new BadRequestException("Leraar bestaat niet"));
 
-            if (scheduledClassRepository.findByDateAndTeacher(localDate, teacher.get()).isPresent()) {
+            if (scheduledClassRepository.findByDateAndTeacher(localDate, teacher).isPresent()) {
                 throw new BadRequestException("De geselecteerde leraar is niet beschikbaar op " +
                         potentialClass.dateAsString() + ".");
             }
 
-            var group = groupRepository.findById(potentialClass.groupId());
-            if (group.isEmpty()) throw new BadRequestException("Groep bestaat niet");
+            var group = groupRepository.findById(potentialClass.groupId())
+                    .orElseThrow(() -> new BadRequestException("Groep bestaat niet"));
 
-            if (scheduledClassRepository.findByDateAndGroup(localDate, group.get()).isPresent()) {
+            if (scheduledClassRepository.findByDateAndGroup(localDate, group).isPresent()) {
                 throw new BadRequestException("Deze groep heeft al een les op " + potentialClass.dateAsString() + ".");
             }
 
-            validClasses.add(new ScheduledClass(group.get(), teacher.get(), localDate));
+            validClasses.add(new ScheduledClass(group, teacher, localDate));
         }
 
         scheduledClassRepository.saveAll(validClasses);
