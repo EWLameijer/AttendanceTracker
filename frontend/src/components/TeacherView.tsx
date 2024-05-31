4eb667333728e1f9b426c053aed940623c647092
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-05-31 19:51:58 +0200

    Changing more code back to original.

--- frontend/src/components/TeacherView.tsx
+++ frontend/src/components/TeacherView.tsx
@@ -5,15 +5,9 @@ import UserContext from "../context/UserContext";
 const TeacherView = () => {
   const loginData = useContext(UserContext);
 
-  const showAll: boolean = true;
-
   return (
     <>
       <h2>Hallo {loginData.username}!</h2>
-      <p>
-        <input type="checkbox" checked={showAll}></input>
-        Toon alle groepen
-      </p>
       <DatePicker />
     </>
   );
